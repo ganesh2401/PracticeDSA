0d0691a1deae9f222ee3b7e425c78b031066de4f
ganesh2401 <43950376+ganesh2401@users.noreply.github.com>
2026-01-10 20:52:01 +0530

    Update ValidAnagram.java

--- ValidAnagram.java
+++ ValidAnagram.java
@@ -3,11 +3,11 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length()) return false;
         
-        int[] data = new int[125];
+        int[] data = new int[26];
 
         for(int i =0; i<s.length();i++){
-            data[s.charAt(i)]++;
-            data[t.charAt(i)]--;
+            data[s.charAt(i)-'a']++;
+            data[t.charAt(i)-'a']--;
         } 
         for(int a:data){
             if(a!=0) return false;
